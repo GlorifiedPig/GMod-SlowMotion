1caf16179d87913ee57187791eedb67199d86cfa
GlorifiedPig <killingpigs123@gmail.com>
2020-07-22 15:10:52 +0200

    p r o g r e s s

--- lua/slowmotion/modules/cl_slowmotion_core.lua
+++ lua/slowmotion/modules/cl_slowmotion_core.lua
@@ -1,5 +1,6 @@
 
-CreateConVar( "slowmotion_multiplier", 0.75, FCVAR_NONE, "This adjusts how slow the slow motion will go whenever you activate it." )
+CreateConVar( "slowmotion_multiplier", 0.5, FCVAR_NONE, "This adjusts how slow the slow motion will go whenever you activate it." )
+CreateConVar( "slowmotion_keybind", "k", FCVAR_NONE, "This is the keybind used to toggle slow motion." )
 
 function SlowMotion.AddToolMenuCategory()
     spawnmenu.AddToolCategory( "Utilities", "SlowMotion", "Slow Motion Mod" )
@@ -8,9 +9,13 @@ end
 function SlowMotion.AddToolMenuOptions()
     spawnmenu.AddToolMenuOption( "Utilities", "SlowMotion", "SlowMotion_Settings", "Settings", "", "", function( panel )
         panel:ClearControls()
-        local slowMotionMultiplier = panel:NumSlider( "Slow Motion Multiplier", "slowmotion_multiplier", 0.01, 1, 2 )
-        slowMotionMultiplier:SetValue( 0.75 )
+        local slowMotionMultiplier = panel:NumSlider( "Slow Motion Multiplier", "slowmotion_multiplier", 0.01, 0.99, 2 )
+        slowMotionMultiplier:SetValue( 0.5 )
         panel:ControlHelp( "This adjusts how slow the slow motion will go whenever you activate it." )
+
+        panel:TextEntry( "Slow Motion Keybind", "slowmotion_keybind" )
+        panel:Button( "Bind", "" )
+        panel:ControlHelp( "This is the keybind used to toggle slow motion. WARNING: Clicking 'Bind' will reset all keys assigned to that key!" )
     end )
 end
 
